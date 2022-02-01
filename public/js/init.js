10dbf41bf21d384091fec5d3dab73ff420bdf8c7
Omer Sadeh <omer.sadeh@protonmail.com>
2022-02-01 13:36:18 +0200

    fix 1

--- public/js/init.js
+++ public/js/init.js
@@ -137,69 +137,14 @@
 /*	contact form
 ------------------------------------------------------*/
 
-var form_id_js = "contactForm";
-
-var data_js = {
-    "access_token": "hr1zve9yhcq1l8c136kvchyi"
-};
-
-function js_onSuccess() {
-   alert("Message Sent. Thank you!");
-   window.location = window.location.pathname;
-}
-
-function js_onError(error) {
-   alert("An error occured. Please try again. (make sure you filled all of the fields)");
-   window.location = window.location.pathname;
-}
-
-var sendButton = document.getElementById("js_send");
-
-function js_send() {
-    sendButton.value='Sending…';
-    sendButton.disabled=true;
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-        if (request.readyState == 4 && request.status == 200) {
-            js_onSuccess();
-        } else
-        if(request.readyState == 4) {
-            js_onError(request.response);
-        }
-    };
-
-    var contactName = document.querySelector("#" + form_id_js + " [name='contactName']").value;
-    var contactEmail = document.querySelector("#" + form_id_js + " [name='email']").value;
-    var subject = document.querySelector("#" + form_id_js + " [name='subject']").value;
-    var message = document.querySelector("#" + form_id_js + " [name='text']").value;
-    if (subject === "") subject = "No Subject";
-    data_js['subject'] = subject;
-    data_js['text'] = message + "\n\nMessage sent from: " + contactName + "\nat: " + contactEmail;
-    var params = toParams(data_js);
-
-    request.open("POST", "https://postmail.invotes.com/send", true);
-    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    request.send(params);
-
-    return false;
-}
-
-sendButton.onclick = js_send;
-
-function toParams(data_js) {
-    var form_data = [];
-    for ( var key in data_js ) {
-        form_data.push(encodeURIComponent(key) + "=" + encodeURIComponent(data_js[key]));
-    }
-
-    return form_data.join("&");
-}
-
-var js_form = document.getElementById(form_id_js);
-js_form.addEventListener("submit", function (e) {
-    e.preventDefault();
-});
+var submitButton = document.getElementById("js_send");
+    var form = document.getElementById("contactForm");
+    form.addEventListener("submit", function (e) {
+        setTimeout(function() {
+            submitButton.value = "Sending...";
+            submitButton.disabled = true;
+        }, 1);
+    });
    
 
 
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -21,30 +21,34 @@ class Contact extends Component {
          <div className="row">
             <div className="eight columns">
 
-               <form id="contactForm" name="contactForm">
+               <form id="contactForm" action="https://postmail.invotes.com/send" method="post">
 					<fieldset>
 
                   <h1><GrMail /></h1><p>{message}</p>
 
                   <div>
 						   <label htmlFor="contactName">Name <span className="required">*</span></label>
-						   <input type="text" defaultValue="" size="35" id="contactName" name="contactName" onChange={this.handleChange} required/>
+						   <input type="text" defaultValue="" size="35" id="contactName" name="extra_contactName" required/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-						   <input type="email" defaultValue="" size="35" id="contactEmail" name="email" onChange={this.handleChange} required/>
+						   <input type="email" defaultValue="" size="35" id="contactEmail" name="extra_email" required/>
                   </div>
 
                   <div>
 						   <label htmlFor="contactSubject">Subject</label>
-						   <input type="text" defaultValue="" size="35" id="contactSubject" name="subject" onChange={this.handleChange}/>
+						   <input type="text" defaultValue="" size="35" id="contactSubject" name="subject" required/>
                   </div>
 
                   <div>
                      <label htmlFor="contactMessage">Message <span className="required">*</span></label>
                      <textarea cols="50" rows="15" id="contactMessage" name="text" required></textarea>
                   </div>
+
+                  <input type="hidden" name="access_token" value="hr1zve9yhcq1l8c136kvchyi" />
+                  <input type="hidden" name="success_url" value=".?message=Email+Successfully+Sent%21&isError=0" />
+                  <input type="hidden" name="error_url" value=".?message=Email+could+not+be+sent.&isError=1" />
 
                   <div>
                      <input type="submit" id="js_send" value="Send" className="submit" />
